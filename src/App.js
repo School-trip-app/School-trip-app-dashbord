f8a53708fdc6cd4b600cb3c797584501e716b8ad
Alhaimouni <m.alhaumouni@gmail.com>
2022-12-18 22:14:39 +0200

    edit navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import Main from './components/Main';
 import Nav from './components/Nav';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Login from './components/Login';
 import cookies from 'react-cookies';
 function App() {
-  const [login , setLogin] = useState(true);
-  useEffect(()=>{
-   if(cookies.load('token')){
-    setLogin(true);
-   }
-  },[])
+  const [login, setLogin] = useState(true);
+  useEffect(() => {
+    if (cookies.load('token')) {
+      setLogin(true);
+    }
+  }, [])
   return (
-  <div className={login?"App-flex":"App"}>
-    {login&&
-      <>
-     <Nav setLogin={setLogin}/>
-     <Main/>
-     </>
-     }
-      {!login&&<Login setLogin={setLogin}/>}
+    <div className={login ? "App-flex" : "App"}>
+      {login &&
+        <>
+          <Nav setLogin={setLogin} />
+          <Main />
+        </>
+      }
+      {!login && <Login setLogin={setLogin} />}
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,56 +1,52 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import logo from '../images/tripadvisor-logo-.png';
+import logo from '../images/logo.png';
 import '../styles/Nav.css';
 import cookies from 'react-cookies';
 
-function Nav({setLogin}) {
-    const handlerLogout=()=>{
-        setLogin(false);
-        cookies.remove('token');
-        cookies.remove('user');
-        cookies.remove('capabilities');
-    }
-    return (
-        <div className='Nav' >
-            <div className='logo'>
-                <img src={logo} alt='logo' width='40' />
-                <span>JOurney</span>
-            </div>
-            <div className='list'>
-                <ul>
-                   <Link to='/'><li>
-                        Dashboard
-                    </li>
-                   </Link>
-                  <Link to='/packages'><li>
-                        Packages
-                    </li>
-                    </Link> 
-                   <Link to='/orders'><li>
-                        Orders
-                    </li>
-                    </Link>
-                   <Link to='/photographers'>
-                    <li>
-                      Photographers  
-                    </li>
-                    </Link> 
-                    <Link to='/products'>
-                    <li>
-                        Products
-                    </li>
-                    </Link>
-                    <li onClick={handlerLogout}>
-                        Log out
-                    </li>
-                </ul>
-                <h3>
-                    Setting
-                </h3>
-            </div>
-        </div>
-    )
+function Nav({ setLogin }) {
+  const handlerLogout = () => {
+    setLogin(false);
+    cookies.remove('token');
+    cookies.remove('user');
+    cookies.remove('capabilities');
+  }
+  return (
+    <div className='Nav' >
+      <div className='logo'>
+        <img src={logo} alt='logo' />
+      </div>
+      <div className='list'>
+        <ul>
+          <Link to='/'><li>
+            Dashboard
+          </li>
+          </Link>
+          <Link to='/packages'><li>
+            Packages
+          </li>
+          </Link>
+          <Link to='/orders'><li>
+            Orders
+          </li>
+          </Link>
+          <Link to='/photographers'>
+            <li>
+              Photographers
+            </li>
+          </Link>
+          <Link to='/products'>
+            <li>
+              Products
+            </li>
+          </Link>
+          <li onClick={handlerLogout}>
+            Log out
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Nav
